f57e403dd316d891a071b6f45bce66b7357bf03b
GabbyYukio <gabbyyukio@gmail.com>
2022-01-13 12:08:01 +0200

    Added p25

---
+++ FPSETUL3/Problema25.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FPSETUL3
+{
+    class interclasare
+    {
+        public static void interclasare_vector (indexare_vector x, indexare_vector y)
+        {
+            int[] v= new int[x.Lungime+y.Lungime];
+            int i = 0, j = 0;
+            for(int k=0; k<=x.Lungime+y.Lungime; k++)
+            {
+                if(x.vector[i]<=y.vector[j])
+                {
+                    v[k] = x.vector[i];
+                    i++;
+                }
+                else
+                {
+                    v[k] = y.vector[j];
+                    j++;
+                }
+            }
+            for(int k=0; i<v.Length; i++)
+                Console.WriteLine("{0} ",v[k]);
+        }
+    }
+    
+    class Problema25
+    {
+        public static void rezolvare()
+        {
+            indexare_vector x = new indexare_vector();
+            indexare_vector y = new indexare_vector();
+            interclasare.interclasare_vector(x, y);
+            UI.run();
+        }
+
+
+
+
+    }
+}
